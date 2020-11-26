63fdb884999c15b41a4eb740773339206757598f
shibeiye <398726681@qq.com>
2020-11-26 15:16:59 +0800

    登录开发完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import router from '@/router'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.prototype.$http = axios
 
 new Vue({
   router,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,11 +2,11 @@
   <div class="container-login">
       <el-card class="my-card">
           <img src="../../assets/logo_index.png" alt="">
-          <el-form :model="loginForm">
-              <el-form-item>
+          <el-form :model="loginForm" :rules="loginRules" ref="loginForm">
+              <el-form-item prop="mobile">
                   <el-input  v-model="loginForm.mobile" placeholder='请输入用户名'></el-input>
               </el-form-item>
-              <el-form-item>
+              <el-form-item prop="code">
                   <el-input  v-model="loginForm.code" placeholder='请输入验证码' style="width:240px;margin-right:8px"></el-input>
                   <el-button>发送验证码</el-button>
               </el-form-item>
@@ -14,22 +14,53 @@
                 <el-checkbox :value="true">已同意</el-checkbox>
               </el-form-item>
               <el-form-item>
-                  <el-button type="primary" style="width:100%;">登录</el-button>
+                  <el-button type="primary" style="width:100%;" @click="login()">登录</el-button>
               </el-form-item>
           </el-form>
       </el-card>
   </div>
 </template>
-
 <script type="text/javascript">
 export default {
   name: 'app-login',
   data () {
+    const checkMobile = (rule, value, callback) => {
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        // 验证成功
+        callback(new Error('手机号格式错误'))
+      } else { // 验证失败
+        callback()
+      }
+    }
     return {
       loginForm: {
         mobile: '',
         code: ''
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码6个字符', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate(valid => {
+        console.log(valid)
+        if (valid) {
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm).then(res => {
+            this.$router.push('/')
+          }).catch(() => {
+            this.$message.error('手机号或验证码错误')
+          })
+        }
+      })
     }
   },
   components: {
